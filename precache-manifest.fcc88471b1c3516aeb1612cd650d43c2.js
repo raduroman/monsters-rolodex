8b1ae677b60aecaf51af62c8888f82acbeb9cba5
Roman <radu-mihai.roman@visma.com>
2020-10-27 22:19:21 +0200

    Updates

---
+++ precache-manifest.fcc88471b1c3516aeb1612cd650d43c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb9bd7ac435f3cf618928be84394d004",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "156a3ab84d3c7058050c",
+    "url": "/monsters-rolodex/static/css/main.fb007b5c.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "156a3ab84d3c7058050c",
+    "url": "/monsters-rolodex/static/js/main.3f2754e7.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
